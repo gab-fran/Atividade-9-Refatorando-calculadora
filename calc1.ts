fae3aba53d2744b033005ec27b12581dd682d475
Gabriel Henrique <gabrielpavao011@gmail.com>
2025-05-20 11:13:16 -0300

    adiciona funções de operação à calculadora

--- calc1.ts
+++ calc1.ts
@@ -23,14 +23,43 @@ function opcoes (opcao:string): void{
 }
 
 function menu():void{
-    console.log(`---* CALCULADORA *---`);
+    console.log(`\n---* CALCULADORA *---`);
     console.log(`Digite `);
     console.log(`+ --> ADIÇÃO`);
     console.log(`- --> SUBTRAÇÃO`);
     console.log(`* --> MULTIPLICAÇÃO`);
     console.log(`/ --> DIVISÃO`);
-    console.log(`0 --> SAIR`);
+    console.log(`0 --> SAIR \n`);
     opcao = teclado(`Digite a sua opção: `);
     opcoes(opcao);
 }
+
 menu(); 
+
+function soma(): void {
+    n1 = parseFloat(teclado("Digite o primeiro número: "));
+    n2 = parseFloat(teclado("Digite o segundo número: "));
+    console.log(`\nO resultado da soma é: ${n1} + ${n2} = ${n1 + n2} \n`);
+    menu();
+}
+
+function subtracao(): void {
+    n1 = parseFloat(teclado("Digite o primeiro número: "));
+    n2 = parseFloat(teclado("Digite o segundo número: "));
+    console.log(`O resultado da subtração é: ${n1} - ${n2} = ${n1 - n2}`);
+    menu();
+}
+
+function multiplicacao(): void {
+    n1 = parseFloat(teclado("Digite o primeiro número: "));
+    n2 = parseFloat(teclado("Digite o segundo número: "));
+    console.log(`O resultado da multiplicação é: ${n1} * ${n2} = ${n1 * n2}`);
+    menu();
+}
+
+function divisao(): void {
+    n1 = parseFloat(teclado("Digite o primeiro número: "));
+    n2 = parseFloat(teclado("Digite o segundo número: "));
+    console.log(`O resultado da divisão é: ${n1} / ${n2} = ${n1 / n2}`);
+    menu();
+}
